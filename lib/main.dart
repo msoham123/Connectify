5fa94e2d63195c41ce7f3ffd600213d1f74d1407
msoham123 <msoham123@gmail.com>
2020-11-29 00:28:56 -0800

    Enabled Firebase, Added Dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:connectify/auth/LandingScreen.dart';
+import 'package:connectify/services/DatabaseService.dart';
+import 'package:connectify/services/FirebaseAuthService.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -14,62 +18,75 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Connectify',
-      theme: ThemeData(
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    return MultiProvider(
+      providers: [
+        Provider<FirebaseAuthService>(
+          create: (_) => FirebaseAuthService(),
+        ),
+        Provider<DatabaseService>(
+          create: (_) => DatabaseService(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Connectify',
+        theme: ThemeData(
 
 
-        backgroundColor: Color.fromRGBO(242, 247, 253, 1),
-        // backgroundColor: Color.fromRGBO(233, 225, 255, 1),
+          backgroundColor: Color.fromRGBO(242, 247, 253, 1),
+          // backgroundColor: Color.fromRGBO(233, 225, 255, 1),
 
-        //Button Colors 
-        buttonColor: Color.fromRGBO(242, 114, 138, 1),
-        hoverColor: Color.fromRGBO(47, 150, 255, 1),
+          //Button Colors
+          buttonColor: Color.fromRGBO(242, 114, 138, 1),
+          hoverColor: Color.fromRGBO(47, 150, 255, 1),
 
 
-        //General Text
-        textTheme: TextTheme(
-          //Main Titles
-            headline1: TextStyle(
+          //General Text
+          textTheme: TextTheme(
+            //Main Titles
+              headline1: TextStyle(
+                fontFamily: 'Muli-Bold',
+                color: Colors.black,
+                fontSize: 20,
+              ),
+
+              //Subtitle
+              subtitle1: TextStyle(
+                fontFamily: 'Muli',
+                color: Colors.black,
+                fontSize: 15,
+              ),
+
+
+             //Button 1 Text
+             button: TextStyle(
+               fontFamily: 'Muli-Bold',
+               color: Colors.white,
+               fontSize: 15,
+             ),
+
+            //Button 2 Text
+            headline4: TextStyle(
               fontFamily: 'Muli-Bold',
-              color: Colors.black,
-              fontSize: 20,
-            ),
-
-            //Subtitle
-            subtitle1: TextStyle(
-              fontFamily: 'Muli',
-              color: Colors.black,
+              color: Colors.blue,
               fontSize: 15,
             ),
 
 
-           //Button 1 Text
-           button: TextStyle(
-             fontFamily: 'Muli',
-             color: Colors.white,
-             fontSize: 15,
-           ),
+            //Textfield
+
+            subtitle2: TextStyle(
+              color:  Colors.grey[600],
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
 
 
-          //Textfield
 
-          subtitle2: TextStyle(
-            color:  Colors.grey[600],
-            fontWeight: FontWeight.bold,
-            fontSize: 15,
           ),
-
-
-
         ),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: MultiProvider(
-          providers: [
-
-          ],
-        child: LandingScreen(),
+        debugShowCheckedModeBanner: false,
+        home: LandingScreen(),
       ),
     );
   }
