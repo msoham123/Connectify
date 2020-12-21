d4f6e27dd5f2ad075c620f7003326dfdf84811a8
msoham123 <msoham123@gmail.com>
2020-12-20 16:13:46 -0800

    Added state and school + user get methods

--- lib/services/DatabaseService.dart
+++ lib/services/DatabaseService.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:connectify/main.dart';
 import 'package:connectify/models/ConnectifyUser.dart';
 
 class DatabaseService {
@@ -23,9 +24,45 @@ class DatabaseService {
   // }
 
 
+  Future<List<String>> getSchools(String state) async{
+    List<String> list = [];
+    await _db.collection("states").doc(state).get().then((snapshot) {
+      snapshot.data().forEach((key, value) {
+        list.add(key);
+      });
+    });
+    print(list);
+    return list;
+  }
+
+  Future<List<String>> getStates() async{
+    List<String> list = [];
+    await _db.collection("states").get().then((snapshot) {
+      for(int i = 0; i < snapshot.docs.length;i++){
+        list.add(snapshot.docs[i].id);
+      }
+    });
+    return list;
+  }
+
+  Future<bool> addToSchool(String state, String school, String uid)async{
+    await _db.collection("users").doc(uid).update({
+      "school": school,
+    });
+    var students = await _db.collection("states").doc(state).get().then((snapshot)=> snapshot.data()[school]);
+    students.add(uid);
+    return await _db.collection("states").doc(state).update({
+      school: students,
+    }).then((value) => true, onError: (value)=> false);
+  }
+
+
   /// Write data
-  Future<void> createUser(String id) {
-    return _db.collection('users').doc(id).set({ /* some data */ });
+  Future<bool> createUser(String id, ConnectifyUser user) async{
+     return await _db
+         .collection('users')
+         .doc(id)
+         .set(user.toJSON(user)).then((value) => true, onError:(value)=> false);
   }
 
 
