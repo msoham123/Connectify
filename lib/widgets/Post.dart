5bb74789c42ba705aec37c38ee3a3c70550ad11f
msoham123 <msoham123@gmail.com>
2020-12-21 23:58:10 -0800

    Created Frontend Post

---
+++ lib/widgets/Post.dart
@@ -0,0 +1,192 @@
+
+
+import 'dart:math';
+
+import 'package:connectify/main.dart';
+import 'package:connectify/models/ConnectifyPost.dart';
+import 'package:connectify/models/ConnectifyUser.dart';
+import 'package:connectify/services/FirestoreService.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:provider/provider.dart';
+
+class Post extends StatefulWidget{
+
+  String description, uid, imageUrl, hashtags;
+  Map<String, String> comments;
+  List<String> stars;
+  ScrollController _controller = ScrollController();
+  DateTime datePublished;
+
+  Post({this.description,this.uid, this.imageUrl, this.comments, this.stars, this.datePublished,this.hashtags});
+
+
+  @override
+  State<StatefulWidget> createState() {
+    return PostState();
+  }
+
+}
+
+class PostState extends State<Post>{
+
+ ConnectifyUser user;
+ bool _inAsyncCall = true;
+ double milliseconds;
+ String time = "";
+
+ @override
+  void initState() {
+   _loadUser();
+    super.initState();
+  }
+
+  void _loadUser()async{
+    user = await Provider.of<FirestoreService>(context, listen: false).getUser(widget.uid);
+    milliseconds = (DateTime.now().millisecondsSinceEpoch-widget.datePublished.millisecondsSinceEpoch)+0.0;
+    time = milliseconds<60000 ? '${(milliseconds/1000).ceil()} seconds ago' : milliseconds < (3.6 * pow(10, 6)) ?
+    '${(milliseconds/60000).ceil()} minutes ago' : milliseconds<(8.64 * pow(10, 7)) ?
+    '${(milliseconds/(3.6 * pow(10, 6))).ceil()} hours ago' : milliseconds<(2.628 * pow(10, 9)) ?
+    '${(milliseconds/(8.64 * pow(10, 7))).ceil()} days ago' : milliseconds<(3.154 * pow(10, 10)) ?
+    '${(milliseconds/(2.628 * pow(10, 9))).ceil()} weeks ago' : '${(milliseconds/(3.154 * pow(10, 10))).ceil()} days ago';
+    if(milliseconds<60000){
+      time = '${(milliseconds/1000).ceil()} seconds ago';
+    }else if( milliseconds < (3.6 * pow(10, 6))){
+      time = '${(milliseconds/60000).ceil()} minutes ago';
+    }else if (milliseconds<(8.64 * pow(10, 7))){
+      time = '${(milliseconds/(3.6 * pow(10, 6))).ceil()} hours ago';
+    }else if (milliseconds<(2.628 * pow(10, 9))) {
+      time = '${(milliseconds/(8.64 * pow(10, 7))).ceil()} days ago';
+    }else if (milliseconds<(3.154 * pow(10, 10))){
+      time = '${(milliseconds/(2.628 * pow(10, 9))).ceil()} weeks ago';
+    }else time = '${(milliseconds/(3.154 * pow(10, 10))).ceil()} days ago';
+    setState(() {
+      _inAsyncCall = false;
+   });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _inAsyncCall ? SizedBox() : Container(
+        width: MediaQuery.of(context).size.width/1.1,
+        decoration: BoxDecoration(
+          color: Theme.of(context).backgroundColor,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              offset: Offset(0.0, 1.0), //(x,y)
+              blurRadius: 2.0,
+            ),
+          ],
+        ),
+        child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height/40,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width/20,
+                    ),
+                    Center(
+                      child: Container(
+                        height: MediaQuery.of(context).size.height/15,
+                        width: MediaQuery.of(context).size.height/15,
+                        decoration: BoxDecoration(
+                          image: _inAsyncCall ? null: DecorationImage(
+                              fit: BoxFit.cover, image: NetworkImage(user.image)),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          color: Theme.of(context).buttonColor,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width/20,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(user.username,
+                          style: Theme.of(context).textTheme.subtitle1.merge(TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+                        ),
+                        Text(
+                          time,
+                          style: Theme.of(context).textTheme.subtitle1.merge(TextStyle(fontSize: 15,)),
+                        ),
+                      ],
+                    ),
+                    Expanded(
+                      child: SizedBox(
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child:  IconButton(
+                            icon: Icon(AntDesign.retweet, color: Theme.of(context).textTheme.subtitle1.color,),
+                            onPressed: (){
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width/20,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height/40,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  image: _inAsyncCall ? null: DecorationImage(
+                      fit: BoxFit.cover, image: NetworkImage(widget.imageUrl)),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Theme.of(context).buttonColor,
+                ),
+                width: MediaQuery.of(context).size.width/1.3,
+                height: MediaQuery.of(context).size.height/3.5,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height/40,
+              ),
+              Row(
+                children: [
+                  FlatButton(
+                    child: Row(
+                      children: [
+                        Icon(AntDesign.star, color: Colors.blue,),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width/40,
+                        ),
+                        Text(
+                          widget.stars!=null ? widget.stars.length.toString() : '0',
+                          style: Theme.of(context).textTheme.subtitle1.merge(TextStyle(fontSize: 15)),
+                        ),
+                      ],
+                    ),
+                    onPressed: (){
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: MediaQuery.of(context).size.width/30),
+                    child: Text(
+                      widget.description,
+                      style: Theme.of(context).textTheme.subtitle1.merge(TextStyle(fontSize: 15)),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height/40,
+              ),
+            ]
+        )
+    );
+  }
+
+}
