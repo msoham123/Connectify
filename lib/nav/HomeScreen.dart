b646ad54e756942f8877912e055781fd4bf6f9c4
msoham123 <msoham123@gmail.com>
2020-12-22 14:49:46 -0800

    Updated List

--- lib/nav/HomeScreen.dart
+++ lib/nav/HomeScreen.dart
@@ -5,11 +5,9 @@ import 'package:connectify/models/ConnectifyPost.dart';
 import 'package:connectify/services/FirestoreService.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:provider/provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connectify/nav/CreatePostScreen.dart';
 import 'package:connectify/services/Dropbox.dart';
 import 'package:connectify/widgets/Post.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
@@ -37,22 +35,33 @@ class HomePageState extends State<HomePage>{
     super.initState();
   }
 
+  @override
+  void dispose() {
+    _controller.dispose();
+    _refreshController.dispose();
+    super.dispose();
+  }
+
 
   void _loadData()async{
     list = [];
     await box.loginWithAccessToken();
     await box.listFolder("");
-    List<ConnectifyPost> temp = await Provider.of<FirestoreService>(context, listen: false).getPosts();
-    for(ConnectifyPost post in temp){
+    var temp = await Provider.of<FirestoreService>(context, listen: false).getPosts();
+    List<ConnectifyPost> newList = temp[0];
+    List<String> idList = temp[1];
+    for(int i = 0; i<newList.length;i++){
       list.add(
           Post(
-            description: post.description,
-            uid: post.uid,
-            imageUrl: await box.getTemporaryLink(post.path),
-            stars: post.stars,
-            comments: post.comments,
-            datePublished: post.datePublished,
-            hashtags: post.hashtags,
+            description: newList[i].description,
+            uid: newList[i].uid,
+            imageUrl: await box.getTemporaryLink(newList[i].path),
+            stars: newList[i].stars,
+            comments: newList[i].comments,
+            datePublished: newList[i].datePublished,
+            hashtags: newList[i].hashtags,
+            postId: idList[i],
+            isImage: newList[i].isImage,
           )
       );
       list.add(SizedBox(height: MediaQuery.of(context).size.height/20,));
@@ -74,24 +83,40 @@ class HomePageState extends State<HomePage>{
         ),
         centerTitle: true,
         backgroundColor: Theme.of(context).buttonColor,
+        leading: IconButton(
+          icon: Icon(AntDesign.reload1, color: Colors.white,),
+          onPressed: (){
+            setState(() {
+              _inAsyncCall = true;
+            });
+            _loadData();
+          },
+        ),
         actions: [
           IconButton(
             icon: Icon(AntDesign.plus, color: Colors.white,),
             onPressed: (){
-              Navigator.push(context, PageTransition(type: PageTransitionType.leftToRightWithFade, child: CreatePostPage()));
+              Navigator.push(context, PageTransition(type: PageTransitionType.rightToLeftWithFade, child: CreatePostPage()));
             },
           ),
         ],
       ),
-      body:   Stack(
+      body: Stack(
         children: [
           Container(
             height: MediaQuery.of(context).size.height/6,
             width: MediaQuery.of(context).size.width,
             color: Theme.of(context).buttonColor,
           ),
-
           SmartRefresher(
+            header: ClassicHeader(
+              textStyle: Theme.of(context).textTheme.button,
+              failedIcon: Icon(Icons.error, color: Theme.of(context).textTheme.button.color),
+              completeIcon: Icon(Icons.done, color: Theme.of(context).textTheme.button.color),
+              idleIcon: Icon(Icons.arrow_downward, color: Theme.of(context).textTheme.button.color),
+              releaseIcon: Icon(Icons.refresh, color: Theme.of(context).textTheme.button.color),
+            ),
+            primary: true,
             controller: _refreshController,
             enablePullDown: true,
             onRefresh: ()async{
@@ -101,27 +126,24 @@ class HomePageState extends State<HomePage>{
               _loadData();
               _refreshController.refreshCompleted();
             },
-            child: CupertinoScrollbar(
+            child: !_inAsyncCall ? CupertinoScrollbar(
+              isAlwaysShown: true,
               controller: _controller,
               child: ListView(
+                primary: false,
+                physics: BouncingScrollPhysics(),
                 controller: _controller,
                 children: [
-
-
-
-                  SizedBox(height: MediaQuery.of(context).size.height/20,),
-
-
-                  !_inAsyncCall ? Column(
-                    children: list,
-                  ) : Center(child: CircularProgressIndicator()),
-
-
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width/40),
+                    child: Column(
+                      children: list,
+                    ),
+                  ),
                 ],
               ),
-            ),
+            ): Center(child: CircularProgressIndicator()),
           ),
-
         ],
       ),
     );
