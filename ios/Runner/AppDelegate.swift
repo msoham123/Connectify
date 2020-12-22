64ac54ba6ea7edceb13354d281adc07113a3f976
msoham123 <msoham123@gmail.com>
2020-12-21 23:51:51 -0800

    Added Dropbox API to IOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import ObjectiveDropboxOfficial
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,3 +13,18 @@ import Flutter
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
+
+ // should be inside AppDelegate class
+        override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+      if let authResult = DBClientsManager.handleRedirectURL(url) {
+          if authResult.isSuccess() {
+              print("dropbox auth success")
+          } else if (authResult.isCancel()) {
+              print("dropbox auth cancel")
+          } else if (authResult.isError()) {
+              print("dropbox auth error \(authResult.errorDescription)")
+          }
+      }
+      return true
+    }
