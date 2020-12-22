39ada2b18597ed886c33169e6c771afbd42ef6d6
msoham123 <msoham123@gmail.com>
2020-12-21 23:54:18 -0800

    Updated User Model to add Startups

--- lib/models/ConnectifyUser.dart
+++ lib/models/ConnectifyUser.dart
@@ -1,42 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ConnectifyUser{
   String username, email, password, school, description;
   List<String> following, followers, posts, messages;
   DateTime dateAccountCreated;
   List<String> saved, groups, notifications;
+  String image;
+  List<String> startups;
 
-  ConnectifyUser({this.username, this.email, this.password, this.school, this.description, this.following, this.followers, this.posts, this.messages, this.dateAccountCreated, this.saved, this.groups, this.notifications});
+  ConnectifyUser({this.username, this.email, this.password, this.school, this.description, this.following, this.followers, this.posts, this.messages, this.dateAccountCreated, this.saved, this.groups, this.startups, this.notifications, this.image});
 
-  factory ConnectifyUser.fromMap(Map data){
+  factory ConnectifyUser.fromJSON(Map<String, dynamic> data){
     data = data ?? {};
     return ConnectifyUser(
-      username: data['username'],
-      email: data['email'],
-      password: data['password'],
-      school: data['school'],
-      description: data['description'],
-      following: data['following'],
-      followers: data['followers'],
-      posts: data['posts'],
-      messages: data['messages'],
-      dateAccountCreated: data['dateAccountCreated'],
-      groups: data['groups'],
-      notifications: data['notifications']
+      username: data['username'] as String,
+      email: data['email'] as String,
+      password: data['password'] as String,
+      school: data['school'] as String,
+      description: data['description'] as String,
+      following: data['following'].cast<String>(),
+      followers: data['followers'].cast<String>(),
+      posts: data['posts'].cast<String>(),
+      messages: data['messages'].cast<String>(),
+      dateAccountCreated: (data['dateAccountCreated'] as Timestamp).toDate(),
+      groups: data['groups'].cast<String>(),
+      saved: data['saved'].cast<String>(),
+      notifications: data['notifications'].cast<String>(),
+      image: data['image'] as String,
+      startups: data['startups'].cast<String>(),
     );
   }
 
 
   Map<String, dynamic> toJSON(ConnectifyUser instance) => <String, dynamic>{
-    'username': username,
-    'email': email,
-    'password': password,
-    'school': school,
-    'description': description,
-    'following': following,
-    'followers': followers,
-    'posts': posts,
-    'messages': messages,
-    'dateAccountCreated': dateAccountCreated,
-    'groups': groups,
-    'notifications': notifications
+    'username': instance.username,
+    'email': instance.email,
+    'password': instance.password,
+    'school': instance.school,
+    'description': instance.description,
+    'following': instance.following,
+    'followers': instance.followers,
+    'posts': instance.posts,
+    'messages': instance.messages,
+    'dateAccountCreated': instance.dateAccountCreated,
+    'groups': instance.groups,
+    'notifications': instance.notifications,
+    'image': instance.image,
+    'saved': instance.saved,
+    'startups': instance.startups,
   };
 }
